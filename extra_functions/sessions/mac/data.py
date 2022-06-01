05fafdd5a2ea03d645af525fe1784e97eecb1250
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-02 07:24:39 +0800

    Delete extra_functions directory

--- extra_functions/sessions/mac/data.py
+++
@@ -1,9 +0,0 @@
-DATA_CHECK = { }
-
-EXPLOITS = {
-        "murus_root": {
-          "function": "murus_root",
-          "os": "macOS",
-          "description": "Get a root shell exploiting vulnerability in 'Murus 1.4.11'"
-        }
-      }
